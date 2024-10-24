aa2099e3de16636fbbefeda28caf3d7401a08615
Hector Gonzalez <hgonzalez542@g.rwu.edu>
2024-10-24 08:57:54 -0400

    Adding a grandson and cousin relaton

--- familyTwo.pl
+++ familyTwo.pl
@@ -19,3 +19,10 @@ male(jim).
 
 mother(X, Y) :- parent(X, Y), female(X).
 father(X, Y) :- parent(X, Y), male(X).
+
+grandson(X, Y) :- parent(P, X), parent(Y, P), male(X).
+
+firstCousin(X, Y) :- parent(P1, X), parent(P2, Y), sibling(P1, P2), X \= Y, \+ sibling(X, Y).
+
+
+
